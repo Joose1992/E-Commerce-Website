f9a7e9a31a0503e20e11894a4c551ed27245b5c7
Jose Montanez <josemontanez1992@gmail.com>
2024-11-05 14:08:45 -0600

    login page created

--- src/app/pages/admin/login/login.component.ts
+++ src/app/pages/admin/login/login.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginModel:any ={
+    userName: '',
+    password: ''
+  }
 
+  onLogin(){
+    console.log(this.loginModel);
+  }
 }
